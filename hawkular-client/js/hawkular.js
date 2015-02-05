8e8b73599839e132ca2d14e6a16f762363fed4dd
Gabriel Cardoso <gabrielc.cardoso@gmail.com>
2015-02-05 16:11:33 -0200

    Button/error position, spinner

--- hawkular-client/js/hawkular.js
+++ hawkular-client/js/hawkular.js
@@ -1,8 +1,20 @@
 jQuery(function($){
 
   $('.btn-primary').click(function(){
-    $('.form-group.input').addClass('has-error');
-    $('.help-block').removeClass('hide');
+    $(this).addClass('disabled');
+    $('.loading').removeClass('hide');
+    
+    setInterval(function () {
+      $('.form-group').addClass('has-error');
+      $('.btn-primary').removeClass('disabled');
+      $('.help-block').removeClass('hide');
+      $('.loading').addClass('hide');
+    },4000);
+  });
+  
+  $('.btn-link').click(function(){
+    $('.loading').addClass('hide');
+    $('.btn-primary').removeClass('disabled');
   });
     
 });
